321f1176817a0f9468d634d9d28ee0a6e10a4a6b
Felipe Nascimento <flpnascto@gmail.com>
2021-06-11 00:20:59 -0300

    create Footer component. Update Header style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="main-container">
       <Header />
       Olá mundo
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <span className="footer-text">© 2021 Suno Movies. All rights reserved.</span>
+    </div>
+  );
+}
+
+export default Footer;
