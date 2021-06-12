54dc180c0e95fe737f6370ac4fc16cac5f224a86
Felipe Nascimento <flpnascto@gmail.com>
2021-06-12 17:39:27 -0300

    create Routes and update App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Router from './Routes'
 import './App.css';
 
 function App() {
   return (
     <div className="main-container">
-      <Header />
-      Olá mundo
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Router />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
