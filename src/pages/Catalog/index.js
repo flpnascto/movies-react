1886554095352a00967bdf1757ce2a98eceb52fa
Felipe Nascimento <flpnascto@gmail.com>
2021-06-14 21:35:48 -0300

    add button load more movies

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Carousel from './Carousel';
 import CatalogHeader from './CatalogHeader';
 import MenuOptions from './MenuOptions';
 import CardView from './CardView';
+import MoviesContext from '../../Context/MoviesContext';
 
 import './style.css';
 
 function Catalog() {
+  const { catalogPage, setCatalogPage } = useContext(MoviesContext);
+
+  const changeCatalogPage = () => {
+    setCatalogPage(catalogPage + 1);
+  }
+
   return (
     <div className="c-content">
       <Carousel />
       <CatalogHeader />
       <MenuOptions />
       <CardView />
+      <button
+        type="button"
+        className="c-button"
+        onClick={() => changeCatalogPage()}
+      >
+        carregar mais
+      </button>
     </div>
   );
 };
